6a7dc69689dc5f19ed4063af2302e0be5b695747
engamo <engamo123@gmail.com>
2023-08-05 11:09:24 +0100

    edited

--- script.js
+++ script.js
@@ -17,12 +17,16 @@
     });
   });
 
-  let ham = document.getElementById('hamburger')
-  function openIcon(){
-    ham.classList.add('w-[100%]');
-  }
-  function closeIcon(){
-    ham.classList.remove('w-[100%]');
-  }
+  // let ham = document.getElementById('hamburger')
+  // function openIcon(){
+  //   ham.classList.add('w-[100%]');
+  // }
+  // function closeIcon(){
+  //   ham.classList.remove('w-[100%]');
+  // }
 
+  function Menu(e) {
+    let list = document.querySelector('ul');
+    e.name === 'menu' ? (e.name = 'close', list.classList.add('top-[80px]') , list.classList.add('opacity-100')) : (e.name = 'menu', list.classList.remove('top-[80px]'), list.classList.add('opacity-100'))
+  }
 
